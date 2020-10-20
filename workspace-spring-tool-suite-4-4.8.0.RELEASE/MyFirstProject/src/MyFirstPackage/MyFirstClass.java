46b3d1af17a14572d0658a363f598c1d97d46f80
Matthew-Daly <matthew.daly40@gmail.com>
2020-10-20 18:57:19 +0700

    MyFirstProject

---
+++ workspace-spring-tool-suite-4-4.8.0.RELEASE/MyFirstProject/src/MyFirstPackage/MyFirstClass.java
@@ -0,0 +1,14 @@
+package MyFirstPackage;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello Again");
+		System.out.println("Hello one more time");
+		
+
+	}
+
+}
