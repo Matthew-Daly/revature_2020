2b78327a94c6133db3ba0948bdb16be0a11b6067
Matthew-Daly <matthew.daly40@gmail.com>
2020-10-21 21:43:25 +0700

    update ArrayWorkshop

--- JavaExamples/ArrayWorkShop/src/example_CRUD/CRUDmain.java
+++ JavaExamples/ArrayWorkShop/src/example_CRUD/CRUDmain.java
@@ -7,6 +7,8 @@ public class CRUDmain {
 	public static void main(String[] args) {
 		
 		int n = 9;
+		int n2 = 9;
+		int n3 = 9;
 		
 //		creating random value array
 		int[] randomArray = new int[10];
@@ -21,13 +23,23 @@ public class CRUDmain {
 //		inserting 0 into randomArray[4]
 		int pos = 4;
 		int value = 0;
-		for (int i = n; i >= pos; i-- ) {
+		for (int i = n2; i >= pos; i-- ) {
 			randomArray[i] = randomArray[i-1];
 		}
 		randomArray[pos - 1] = value;
-		n++;
+		n2++;
 		
-		System.out.print(Arrays.toString(randomArray));	
+		System.out.print("\nPrinting after insertion..." + Arrays.toString(randomArray));
+		
+		
+//		deleting element
+		int delpos = 2;
+		for (int i = delpos - 1; i <= n3; i++) {
+			randomArray[i]=randomArray[i+1];
+		}
+		n3--;
+		System.out.print("\nPrinting after deletion..." + Arrays.toString(randomArray));		
+		
 	}
 
 }
