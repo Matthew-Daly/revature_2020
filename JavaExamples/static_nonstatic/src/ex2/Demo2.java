124a8234d0ff449b4dc04bbefd9d0dca05fccf53
Matthew-Daly <matthew.daly40@gmail.com>
2020-10-26 07:28:20 +0700

    add workshop

---
+++ JavaExamples/static_nonstatic/src/ex2/Demo2.java
@@ -0,0 +1,32 @@
+package ex2;
+
+import ex3.Animal;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+//		Calculator.helloStaticCalci();
+		
+		Animal.helloStaticAnimal1();
+		Animal.helloStaticAnimal2();
+		
+//		Calculator c=new Calculator();
+//		c.helloNonStaticCalci();
+		
+		Animal a =new Animal();
+		a.helloNonStaticAnimal();
+		
+//		int result=c.add(100, 22);
+//		System.out.println("result = "+result);
+//		
+//		System.out.println("result = "+c.add(99, 888));
+		
+//		System.out.println("result(a+b+c) =  "+c.add(100, 22, 222));
+		
+//		System.out.println("result(float,float) = "+c.add(99.9f, 88.8f));
+		
+//		c.add(100, 100, 220, 330);
+	}
+
+}
---
+++ JavaExamples/static_nonstatic/src/ex3/Animal.java
@@ -0,0 +1,17 @@
+package ex3;
+
+public class Animal {
+
+	public static void helloStaticAnimal1() {
+		System.out.println("Hello from helloStaticAnimal1()");
+	}
+	
+	public static void helloStaticAnimal2() {
+		System.out.println("Hello from helloStaticAnimal2()");
+	}
+	
+	public void helloNonStaticAnimal() {
+		System.out.println("Hello from helloNonStaticAnimal()");
+	}
+}
+
