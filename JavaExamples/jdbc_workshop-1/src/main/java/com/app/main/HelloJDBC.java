0255e15cadd36a1c11fd67212f37f679aa4cc0d0
Matthew-Daly <matthew.daly40@gmail.com>
2020-10-31 00:24:35 +0700

    update jdbc_workshop

--- JavaExamples/jdbc_workshop-1/src/main/java/com/app/main/HelloJDBC.java
+++ JavaExamples/jdbc_workshop-1/src/main/java/com/app/main/HelloJDBC.java
@@ -2,7 +2,9 @@ package com.app.main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class HelloJDBC {
 
@@ -18,10 +20,32 @@ public class HelloJDBC {
 			String username="postgres";
 			String password="Gain45dig";
 			connection=DriverManager.getConnection(url, username, password);
-			System.out.println("Connection Successfull");
+			System.out.println("Connection Successfull!");
+			
+			//step 3 - Create statement
+			Statement statement=connection.createStatement();
+			String sql="select id,name,age,gender,teamName,contact from roc_revature.player";
+			
+			System.out.println("Statment Created!");
+			
+			//step 4 - Execute Query 
+			ResultSet rs=statement.executeQuery(sql); 	
+			System.out.println("Queary Exectued!");
+			
+			//step 5 Process Results
+			while(rs.next()) {
+				System.out.println("ID = " + rs.getInt("id"));
+				System.out.println("Name = " + rs.getString("name"));
+				System.out.println("Gender = " + rs.getString("gender"));
+				System.out.println("TeameName = " + rs.getString("teamName"));
+				System.out.println(("Contact = " + rs.getLong("contact") + "\n"));
+			}
+			System.out.println("Results Processed");
+			
+			
 			
 		} catch (ClassNotFoundException e) {
-			System.out.println(e);;
+			System.out.println(e);;	
 		} catch (SQLException e) {
 			System.out.println(e);
 		} finally {
